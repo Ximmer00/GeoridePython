524b9dde1884bbd0c3ff0546bef32fc2eeb2fefc
BONNARD Loic <loic.bonnard@eolas.fr>
2020-01-17 15:05:13 +0100

    Script fonctionnel Manque le passage d'arguments

--- Script_CLI.py
+++ Script_CLI.py
@@ -1,179 +1,154 @@
 #!/usr/bin/env python3
 
-import request
+import requests
+import re
+import sys
+import getopt
 
-#Init variables
+# Init variables
 GEORIDE_API_HOST = "https://api.georide.fr"
 GEORIDE_API_ENDPOINT_LOGIN = "/user/login"
-GEORIDE_API_ENDPOINT_NEW_TOKEN = "/user/new-token"
-GEORIDE_API_ENDPOINT_LOGOUT = "/user/logout"
-GEORIDE_API_ENDPOINT_USER = "/user"
 GEORIDE_API_ENDPOINT_TRAKERS = "/user/trackers"
-GEORIDE_API_ENDPOINT_TRIPS = "/tracker/:trackerId/trips"
 GEORIDE_API_ENDPOINT_LOCK = "/tracker/:trackerId/lock"
 GEORIDE_API_ENDPOINT_UNLOCK = "/tracker/:trackerId/unlock"
 GEORIDE_API_ENDPOINT_TOGGLE_LOCK = "/tracker/:trackerId/toggleLock"
-GEORIDE_API_ENDPOINT_POSITIONS = "/tracker/:trackerId/trips/positions"
-GEORIDE_API_ENDPOINT_TRIP_SHARE = "/tracker/:trackerId/share/trip"
+
 
 ################################################################################
 ################################################################################
 #########################   Functions for remote requests ######################
 
-def request_to_api( sub_url, reqType, head, encoded_data ):
-    if reqType == 'POST' :
-        response = requests.post(GEORIDE_API_HOST + sub_url, data=encoded_data, headers={'Content-Type': 'application/json'})
+def request_to_api(sub_url, reqType, *args, **kwargs):
+    """fonction de requete a l'api"""
+    head = kwargs.get('header', None)
+    encoded_data = kwargs.get('data', None)
+
+    if reqType == 'POST':
+        response = requests.post(GEORIDE_API_HOST + sub_url, data=encoded_data,
+                                 headers={'Content-Type': 'application/json'})
     elif reqType == 'GET':
         response = requests.get(GEORIDE_API_HOST + sub_url, headers=head)
     else:
-        print("ALERTE")
+        print("ALERTE !!")
 
     if response:
-        return response    #returning the response decoded
+        return response  # returning the response decoded
     else:
-        print(r.status_code)           #Stopping programm with the error
-        sys.exit
+        print(r.status_code)  # Stopping programm with the error
+        sys.exit()
 
 
-def get_auth_header (token):
+def get_auth_header(token):
+    """Retourne le header avec l'Authorization token """
     return{"Authorization": "Bearer " + token}
 
 
-def get_token (email, password):
-    #function to getting the account token (available 30 days)
+def get_token(email, password):
+    """Récupère le token d'authentificatio"""
+    # function to getting the account token (available 30 days)
     data = {'email': email, 'password': password}
     encoded_data = json.dumps(data).encode('utf-8')
-    request_to_api(sub_url=GEORIDE_API_ENDPOINT_LOGIN, reqType='POST', encoded_data=encoded_data )
-    content = parse_json(response)    #transforming reponse in hash
-    return content->{'authToken'}         #giving back the token
+    request_to_api(GEORIDE_API_ENDPOINT_LOGIN,'POST', data=encoded_data)
+    content = response.json()  # transforming reponse in hash
+    return content['authToken']  # giving back the token
 
-def get_trackers (auth_header):    #This def needs improvement about listing of trackers
-    content     = request_to_api( '/user/trackers', 'GET', auth_header )
-    response    = parse_json(content)
-    # print Dumper(response)
-    if ( response->[0]->{'canLock'} ) {
 
-        # print("Can lock ", response->[0]->{'trackerName'}."\n")
-        return response->[0]
-    }
-    else {
-        print( "Cannot lock ", response->[0]->{'trackerName'}, "\n" )
-    }
-}
+def get_trackers(auth_header):  # This def needs improvement about listing of trackers
+    """Récupère la liste des trackers que l'user peut lock"""
+    content = request_to_api(GEORIDE_API_ENDPOINT_TRAKERS, 'GET', header=auth_header)
+    response = content.json()
+    trackers = []
+    for json_tracker in response_data:
+        if json_tracker['canLock']:
+            trackers.append(GeorideTracker.from_json(json_tracker))
+    return trackers
 
-def toggle_tracker (tracker_id, bearer_header){
-    request_to_api( '/tracker/' . tracker_id . '/toggleLock',
-        'POST', bearer_header )
-}
 
-def lock_tracker (tracker_id, bearer_header){
-    request_to_api( '/tracker/' . tracker_id . '/lock',
-        'POST', bearer_header )
-}
+def toggle_tracker(tracker_id, bearer_header):
+    response = request_to_api(GEORIDE_API_HOST + GEORIDE_API_ENDPOINT_TOGGLE_LOCK.replace(':trackerId', str(tracker_id)), 'POST', head=bearer_header)
+    response_data = response.json()
+    return response_data['locked']
 
-def unlock_tracker (tracker_id, bearer_header) {
-    request_to_api( '/tracker/' . tracker_id . '/unlock',
-        'POST', bearer_header )
-}
 
-def generate_token (email, password){
+def lock_tracker(tracker_id, bearer_header):
+    response = request_to_api(GEORIDE_API_HOST + GEORIDE_API_ENDPOINT_LOCK.replace(':trackerId', str(tracker_id)), head=bearer_header)
+    if response.status_code != 204:
+        return False
+    return True
 
-    #say "generate_token def"
-    encrypted_token = read_conf(email)->{token}
-    token           = decrypt_string( encrypted_token, password )
-    auth_header     = get_auth_header(token)
-    response = request_to_api( '/user/new-token', 'GET', auth_header )
-    newToken = parse_json(response)
-    new_encrypted_token =
-      encrypt_string( newToken->{'authToken'}, password )
-    update_conf( email, 'token', new_encrypted_token )
-}
 
-def get_pos (bearer_header){
-    tracker       = get_trackers(bearer_header)
-    return ( tracker->{'latitude'}, tracker->{'longitude'} )
-}
+def unlock_tracker(tracker_id, bearer_header):
+    response = request_to_api(GEORIDE_API_HOST + GEORIDE_API_ENDPOINT_UNLOCK.replace(':trackerId', str(tracker_id)), head=bearer_header)
+    if response.status_code != 204:
+        return False
+    return True
+
+
+def get_pos(bearer_header):
+    tracker_raw = get_trackers(bearer_header)
+    tracker = tracker_raw.json()
+    return tracker[0]['latitude'], tracker[0]['longitude']
 
 ################################################################################
 ################################################################################
 ####################  Functions for this script (local) ########################
 
-def show_loc (lat, lon){
-    url = "https://www.google.com/maps/search/?api=1&query=lat,lon"
-    say "Well, this are the coordinates of your the tracker : \nLatitude = lat,\t longitude = lon"
-    say "Url to access with google : url"
-}
+def show_loc(lat, lon):
+    url = f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
+    print(f"Well, this are the coordinates of your the tracker : \nLatitude = {lat},\t longitude = {lon}")
+    print(f"Url to access with google : {url}")
 
 
-def printing_state (tracker_state, tracker_name){
-    state
-    state = "lock" if tracker_state == 1 else state = "unlock"
-    print("%s is %s" % tracker_name state)
-}
+def printing_state(tracker_state, tracker_name):
+    # state = "lock" if tracker_state else state = "unlock"
+    state = ("unlock", "lock")[tracker_state]
+    print("%s is %s" % tracker_name, state)
 
-def show_status {
-    auth_header  = shift
-    tracker      = get_trackers(auth_header)
-    tracker_name = tracker->{'trackerName'}
-    kilometers   = tracker->{'odometer'} / 1000
-    if ( tracker->{'isLocked'} ) {
-        printing_state( 1, tracker_name )
-    }
-    else {
-        printing_state( 0, tracker_name )
-    }
+def show_status(auth_header):
+    tracker = get_trackers(auth_header)
+    tracker_name = tracker[0]['trackerName']
+    kilometers = tracker[0]['odometer'] / 1000
+    if (tracker[0]['isLocked']):
+        printing_state(1, tracker_name)
+    else:
+        printing_state(0, tracker_name)
     rounded = int(kilometers)
-    final   = commify(rounded)
-    print "tracker_name has final km\n\n"
-}
+    final = commify(rounded)
+    print(f"{tracker_name} has {final} km")
 
-def commify {
-    text = reverse _[0]
-    text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/1 /g
-    return scalar reverse text
-}
+def commify (text):
+    return re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1,", "%d" % text)
 
-def config_main {
 
-    #say "config_main def"
-    #Entering main !!
+def config_main():
     email_main = ARGV[0]
-    if (email_main !~  m/^[\w_\.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,8}/i){
-      say "Email is not an correct, exiting .."
-      exit 1
-    }
+    if not (re.match(r"^[\w_\.]+@[a-zA-Z]+?\.[a-zA-Z]{1,6}",email_main)):
+      print("Email is not an correct, exiting ..")
+      sys.exit(1)
     password_main = ARGV[1]
-    return ( email_main, password_main )
-}
+    return email_main, password_main
 
-def command_treat {
-    ( command, auth_header, tracker_id ) = @_
-    if ( command eq "lock" ) {
-        lock_tracker( tracker_id, auth_header )
-        say "Locked !"
-    }
-    elsif ( command eq "unlock" ) {
-        unlock_tracker( tracker_id, auth_header )
-        say "Unlocked !"
-    }
-    elsif ( command eq "status" ) {
+def command_treat(command, auth_header, tracker_id):
+    if (command == "lock"):
+        lock_tracker(tracker_id, auth_header)
+        print("Locked !")
+    elif(command == "unlock"):
+        unlock_tracker(tracker_id, auth_header)
+        print("Unlocked !")
+    elif(command == "status"):
         show_status(auth_header)
-    }
-    elsif ( command eq "locate" ) {
+    elif(command == "locate"):
         show_loc(get_pos(auth_header))
-    }
-}
 
-def Main {
-    ( mail, pass, exists ) = config_main()
-    ( tracker_name, tracker_id )
-    header = get_auth_header( get_token( mail, pass ) )
+
+
+def Main():
+    mail, password = config_main()
+    header = get_auth_header(get_token(mail, password))
     tracker_raw = get_trackers(header)
-    tracker_name = tracker_raw->{'trackerName'}
-    tracker_id   = tracker_raw->{'trackerId'}
+    tracker_name = tracker_raw[0]['trackerName']
+    tracker_id = tracker_raw[0]['trackerId']
     command = ARGV[2]
-    command_treat( command, header, tracker_id )
-}
-
+    command_treat(command, header, tracker_id)
 
 Main()
